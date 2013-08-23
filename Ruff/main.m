26cc5938e1ac6247733d0961a2949513e7cb2da6
Troy Stump <troystump@gmail.com>
2013-08-22 22:11:35 -0700

    initial commit.

---
+++ Ruff/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ruff
+//
+//  Created by Troy Stump on 8/12/13.
+//  Copyright Troy Stump 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
